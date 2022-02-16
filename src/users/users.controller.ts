428a13450bef316153e2bc956d2bb895b91a4f25
Trung Nguyen <trung1704ptit@gmail.com>
2022-02-16 11:32:21 +0700

    add user model

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user-request.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+
+    @Post()
+    async createUser(
+        @Body() createUserRequest: CreateUserRequest
+    ): Promise<any> {
+        return this.usersService.createUser(createUserRequest)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user-request.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly userRepository: UsersRepository) {}
+    async createUser(createUserRequest: CreateUserRequest):  Promise<any> {
+        return this.userRepository.insertOne(createUserRequest)
+    }
+}
